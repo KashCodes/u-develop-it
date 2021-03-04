2142e41a51ebfa6adb8673fcbf33bceed57cfcd4
Kashay Arbelo <kashay@agyeman.me>
2021-03-04 06:47:57 -0700

    added voter by id route

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -5,7 +5,7 @@ const inputCheck = require('../../utils/inputCheck');
 
 // get all voters
 router.get('/voters', (req, res) => {
-  const sql = `SELECT * FROM voters`;
+  const sql = `SELECT * FROM voters ORDER BY last_name`;
   const params = [];
 
   db.all(sql, params, (err, rows) => {
@@ -17,6 +17,24 @@ router.get('/voters', (req, res) => {
     res.json({
       message: 'success',
       data: rows
+    });
+  });
+});
+
+// api route for individual voters by id 
+router.get('/voter/:id', (req, res) => {
+  const sql = `SELECT * FROM voters WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.get(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: row
     });
   });
 });
