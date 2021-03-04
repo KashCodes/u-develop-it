e354fb8718fef48e3bbbc3db7ab0da299e0f3c56
Kashay Arbelo <kashay@agyeman.me>
2021-03-04 06:13:36 -0700

    added api to update candidates party id

--- server.js
+++ server.js
@@ -120,6 +120,31 @@ app.post('/api/candidate', ({ body }, res) => {
   });
 });
 
+// update party id's in candidates 
+app.put('/api/candidate/:id', (req, res) => {
+  const errors = inputCheck(req.body, 'party_id');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+  const sql = `UPDATE candidates SET party_id = ? 
+               WHERE id = ?`;
+  const params = [req.body.party_id, req.params.id];
+
+  db.run(sql, params, function(err, result) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: req.body,
+      changes: this.changes
+    });
+  });
+});
+
 // api route to select parties table
 app.get('/api/parties', (req, res) => {
   const sql = `SELECT * FROM parties`;
